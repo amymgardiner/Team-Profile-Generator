1d1e15caa8ba39ee028d59e3e04cc98d0acf156d
Amy Gardiner <amymgardiner@gmail.com>
2022-07-23 13:11:37 -0500

    first employee test passed

--- __tests__/Employee.test.js
+++ __tests__/Employee.test.js
@@ -1,7 +1,7 @@
 const Employee = require('../lib/Employee')
 
 test('creates employee object', () => {
-    const employee = new Employee('Amy', '42', 'amymgardiner@gmail.com');
+    const employee = new Employee('Amy', 42, 'amymgardiner@gmail.com');
 
     expect(employee.name).toEqual(expect.any(String));
     expect(employee.employeeId).toEqual(expect.any(Number));

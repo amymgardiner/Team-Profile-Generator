a54a66d377538f7a65d032c796d917795309f653
Amy Gardiner <amymgardiner@gmail.com>
2022-07-22 21:45:53 -0500

    starting to link the manager, engineer, and intern objects to the app

--- __tests__/Employee.test.js
+++ __tests__/Employee.test.js
@@ -1,1 +1,9 @@
 const Employee = require('../lib/Employee')
+
+test('creates employee object', () => {
+    const employee = new Employee('Amy', '42', 'amymgardiner@gmail.com');
+
+    expect(employee.name).toEqual(expect.any(String));
+    expect(employee.employeeId).toEqual(expect.any(Number));
+    expect(employee.email).toEqual(expect.any(String));
+});
--- app.js
+++ app.js
@@ -2,6 +2,10 @@ const inquirer = require('inquirer');
 const fs = require('fs');
 const generateProfile = require('./lib/generateProfile');
 
+const Manager = require('./lib/Manager');
+const Engineer = require('./lib/Engineer');
+const Intern = require('./lib/Intern');
+
 const teamArray = [];
 
 const addManager = () => {
